f83dab378c84728ccb2ef0b836cd5f4acbc4eef2
uchichabrandon <41881828+uchichabrandon@users.noreply.github.com>
2020-04-14 20:53:48 -0400

    add sort elems to q chal and butn links to drivthr

--- js/pals_drive_thru.js
+++ js/pals_drive_thru.js
@@ -1,3 +1,14 @@
+//open queue challenge page on button click
+$("#challenge-button").on("click",function(){
+  window.location.href="queue_challenge.html";
+});
+
+//open resource page for queues on button click
+$("#queue-resource-button").on("click",function(){
+  window.open("https://www.tutorialspoint.com/data_structures_algorithms/dsa_queue.htm");
+});
+
+//set properites of sortable draggable cubes
 $(".cube-constraint").sortable({
   containment: ".cube-constraint",
   axis: "x",
@@ -6,12 +17,10 @@ $(".cube-constraint").sortable({
   cursor: "grabbing",
   snapTolerance: 30
 });
-
+//make sure nothing shows in background when sortable is dragged
 $( function() {
   $( "#sortable" ).sortable({
     placeholder: ""
   });
   $( "#sortable" ).disableSelection();
 } );
-
-alert($(".cube-constraint").find("p").innerHTML())
