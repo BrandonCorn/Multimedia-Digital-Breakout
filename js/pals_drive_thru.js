1267e78a1f8b8351afc64e6a0125eea1082a3164
uchichabrandon <41881828+uchichabrandon@users.noreply.github.com>
2020-04-15 21:14:13 -0400

    added random num hint js

--- js/pals_drive_thru.js
+++ js/pals_drive_thru.js
@@ -10,13 +10,13 @@ $("#queue-resource-button").on("click",function(){
   window.open("https://www.tutorialspoint.com/data_structures_algorithms/dsa_queue.htm");
 });
 
-$("#form-random-num").on("click",function(e){
+$("#random-number").on("click",function(e){
   e.preventDefault();
 });
 
 $("#form-answer").on("click",function(e){
   e.preventDefault();
-})
+});
 
 //set properites of sortable draggable cubes
 $(".cube-constraint").sortable({
@@ -34,3 +34,62 @@ $( function() {
   });
   $( "#sortable" ).disableSelection();
 });
+
+//constants
+const c = {
+  num : Math.floor((Math.random() * 20) + 1),
+  count : 0,
+  increment : function(){
+    this.count++;
+  }
+};
+
+//upon clicking this button you submit your answer to be analyzed
+//It will be check to make sure it is a digit and is within the range of guesses
+//Once validation is accepted, input is compared.
+$("#random-number").on('click',function(){
+  checkRandomNum();
+});
+
+function checkRandomNum(){
+  $("#random-number").popover("dispose");
+  var pop;
+  var correct = false;
+  var regex = /[0-9 -()+]+$/;
+  var numInputForRegex = $("#user-num-guess").val();
+  //input validation
+  if (c.count >=3){
+    pop = $("#random-number").popover({content: "You have no more guesses left",});
+    pop.popover("show");
+    $("#random-number").prop("disabled",true);
+  }
+  else if (!(regex.test(numInputForRegex))){
+    pop = $("#random-number").popover({content: "Not a valid entry, enter a number",});
+    pop.popover("show");
+  }
+  else if (numInputForRegex > 20 || numInputForRegex < 0){
+    pop = $("#random-number").popover({content: "Not a valid entry, number must be between 1 and 20",});
+    pop.popover("show");
+  }
+  else{
+    c.increment();
+    var userNum = parseInt($("#user-num-guess").val());
+    if(userNum < c.num){
+      pop = $("#random-number").popover({content: userNum + " is lower than the random number",});
+      pop.popover("show");
+      $("#form-random-num").trigger("reset");
+    }
+    else if (userNum > c.num){
+      pop = $("#random-number").popover({content: userNum + " is higher than the random number",});
+      pop.popover("show");
+      $("#form-random-num").trigger("reset");
+    }
+    else{
+      correct = true;
+      pop = $("#random-number").popover({content: "Congratulations, you got it",});
+      pop.popover("show");
+      $("#form-random-num").trigger("reset");
+      $("#challenge-hint").removeClass("invisible");
+    }
+  }
+};
