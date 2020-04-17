33a4b9840b61befa344e456cca991712aa85a361
uchichabrandon <41881828+uchichabrandon@users.noreply.github.com>
2020-04-16 23:35:20 -0400

    finished validation for q2

--- js/pals_drive_thru.js
+++ js/pals_drive_thru.js
@@ -1,20 +1,32 @@
 //import {checkRandomNum} from 'comic_con.js';
 
-//open queue challenge page on button click
+//open queue challenge page on button click, belongs to story page
 $("#challenge-button").on("click",function(){
   window.location.href="queue_challenge.html";
 });
 
-//open resource page for queues on button click
+//open resource page for queues on button click, belongs to story page
 $("#queue-resource-button").on("click",function(){
   window.open("https://www.tutorialspoint.com/data_structures_algorithms/dsa_queue.htm");
 });
 
-$("#random-number").on("click",function(e){
+//prevent form submission of question 1 answer
+$("#question-1-form").on("click",function(e){
   e.preventDefault();
 });
 
-$("#form-answer").on("click",function(e){
+//prevent form submission of question 2 answer
+$("#question-2-form").on("click",function(e){
+  e.preventDefault();
+});
+
+//prevent form submission guessing number for hint
+$("#form-random-num").on("click",function(e){
+  e.preventDefault();
+});
+
+//prevent form submission submitting code to next challenge
+$("#code-form").on("click",function(e){
   e.preventDefault();
 });
 
@@ -25,10 +37,15 @@ const c = {
   increment : function(){
     this.count++;
   },
-  answerCarQueue : ["Black Car", "You", "Blue Car", "Red Truck", "Silver Car"],
-  code: Math.floor((Math.random() * 899) + 100)
+  answers : {
+    answerCarQueue : ["Black Car", "You", "Blue Car", "Red Truck", "Silver Car"],
+    q1 : ["arrays", "linked-lists", "pointers", "structures"],
+    q2 : "peek"
+  },
+  code : Math.floor((Math.random() * 899) + 100)
 };
 
+//make list sortable
 $( function() {
   $( "#sortable" ).sortable({
     placeholder: "",
@@ -37,6 +54,7 @@ $( function() {
   $( "#sortable" ).disableSelection();
 });
 
+//on stop event, check elements for correct order and provides code if successful
 var sortableListValidation = function(event,ui){
   var ogCarListElems = $("#sortable").children();
   //alert(ogCarListElems);
@@ -44,37 +62,53 @@ var sortableListValidation = function(event,ui){
   for (const element of ogCarListElems){
     currCarQueue.push(element.innerHTML);
   }
-  if (validateQueue(currCarQueue)){
-    successMessageCode("#success-message");
+  if (ValidateQueue(currCarQueue)){
+    SuccessMessageCode("#success-message",0);
   }
 };
 
-function validateQueue(elements){
+//returns true if all elments in the queue are in the correct order
+function ValidateQueue(elements){
   var correct = true;
   for (var i = 0; i < elements.length; i++){
-    if (elements[i] != c.answerCarQueue[i]){
+    if (elements[i] != c.answers.answerCarQueue[i]){
       correct = false;
     }
   }
   return correct;
-};
+}
 
-function successMessageCode(id){
+//creates a random success message for the queue completion
+function SuccessMessageCode(id,pos){
   const success = ["Great Job! ","You did it! ", "Perfect! ", "Nice Work! "];
   const i = Math.floor((Math.random() * 4) + 1);
-  $(id).text(success[i] + "The code is: " + c.code.toString()[0]);
+  $(id).text(success[i] + "The code is: " + c.code.toString()[pos]);
   $(id).removeClass("invisible");
 }
 
-
-//upon clicking this button you submit your answer to be analyzed
-//It will be check to make sure it is a digit and is within the range of guesses
-//Once validation is accepted, input is compared.
-$("#random-number").on('click',function(){
-  checkRandomNum();
+//validate question 1
+$("#q-2-button").on("click",function(){
+  ValidateQuestionTwo();
 });
 
-function checkRandomNum(){
+function ValidateQuestionTwo(){
+  if (c.answers.q2 == $("#q-2-answer").val().toLowerCase()){
+    console.log(true);
+  }
+  else {
+    console.log(false);
+  }
+}
+
+
+//button click calls checkRandom to validate user answer for hint
+$("#random-number").on('click',function(){
+  CheckRandomNum();
+});
+
+//checks input isdigit, is within range, and user has guesses left
+//Once validation is accepted, input is compared.
+function CheckRandomNum(){
   $("#random-number").popover("dispose");
   var pop;
   var correct = false;
@@ -82,8 +116,8 @@ function checkRandomNum(){
   var numInputForRegex = $("#user-num-guess").val();
   //input validation
   if (c.count >=3){
-    pop = $("#random-number").popover({content: "You have no more guesses left", trigger : "hover", delay : {"show" : 500, "hide" : 100},});
-    //pop.popover("show");
+    pop = $("#random-number").popover({content: "You have no more guesses left", trigger : "hover", delay : {"hide" : 0},});
+    pop.popover("show");
     $("#random-number").prop("disabled",true);
   }
   else if (!(regex.test(numInputForRegex))){
@@ -112,6 +146,7 @@ function checkRandomNum(){
       pop = $("#random-number").popover({content: "Congratulations, you got it", trigger: "hover", delay: {"hide": 0},});
       pop.popover("show");
       $("#form-random-num").trigger("reset");
+      $("#challenge-hint").text("Hint: The second digit of the code is " + c.code.toString()[1]);
       $("#challenge-hint").removeClass("invisible");
     }
   }
