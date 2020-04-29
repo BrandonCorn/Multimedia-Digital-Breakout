79f1d8233befe96f43851ff294284cd64965e20c
uchichabrandon <41881828+uchichabrandon@users.noreply.github.com>
2020-04-29 19:15:44 -0400

    no longer using success messages or code for q 1-3

--- pals-story/pals.js
+++ pals-story/pals.js
@@ -3,6 +3,7 @@ window.addEventListener('load',function(){
   if(chef.get("reachstory2") == "false"){
     chef.set("reachstory2","true");
   }
+  SetSuccessValues();
 });
 //open queue challenge page on button click, belongs to story page
 $("#challenge-button").on("click",function(){
@@ -60,6 +61,23 @@ function SetSuccessValues(){
   chef.set("challenge-2-question-3","false");
 }
 
+function CheckAllQuestions(){
+  const chef = B();
+  const a = chef.get("challenge-2-queue-complete");
+  const b = chef.get("challenge-2-question-1");
+  const c = chef.get("challenge-2-question-2");
+  const d = chef.get("challenge-2-question-3");
+  if (a == "true" && b == "true" && c == "true" && d == "true"){
+    Swal.fire({
+      title:'Great work!',
+      text:'You\'re getting the hang of it! Now let\'s make our way to Colorado to hit the slopes with Adeline!',
+      icon:'success'
+    }).then(function(){
+      window.location.href = '../breckenridge-story/binary-tree-breckenridge.html';
+    });
+  }
+}
+
 //make list sortable
 $( function() {
   $( "#sortable" ).sortable({
@@ -71,14 +89,15 @@ $( function() {
 
 //on stop event, check elements for correct order and provides code if successful
 var sortableListValidation = function(event,ui){
-  var c = C();
-  var ogCarListElems = $("#sortable").children();
-  var currCarQueue = [];
+  const c = C();
+  const ogCarListElems = $("#sortable").children();
+  let currCarQueue = [];
   for (const element of ogCarListElems){
     currCarQueue.push(element.innerHTML);
   }
   if (ValidateQueue(currCarQueue)){
     SuccessMessageCode(c.mID.a,0);
+    CheckAllQuestions();
   }
 };
 
@@ -94,13 +113,14 @@ function SuccessMessageCode(id,pos){
 
 //returns true if all elments in the queue are in the correct order
 function ValidateQueue(elements){
-  var answer = ["Black Car", "You", "Blue Car", "Red Truck", "Silver Car"];
-  var correct = true;
+  const chef = B();
+  const answer = ["Black Car", "You", "Blue Car", "Red Truck", "Silver Car"];
+  let correct = true;
   for (var i = 0; i < elements.length; i++){
     //if (elements[i] != o.answers.answerCarQueue[i]){
     if (elements[i] != answer[i]){
       correct = false;
-      var chef = B().set("challenge2",(Math.floor(Math.random() * 899) + 100));
+      chef.set("challenge-2-queue-complete","true");
     }
   }
   return correct;
@@ -109,22 +129,26 @@ function ValidateQueue(elements){
 //validation uses regex
 function ValidateQuestionOne(input){
   const chef = B();
-  const regEx = new RegExp("(linked(-| )\?lists\?)|(arrays\?)|(pointers\?)|(structures\?)","gi")
+  const regEx = new RegExp("(linked(-| )\?lists\?)|(arrays\?)|(pointers\?)|(structures\?)","gi");
   //const answer = $("#q-1-answer").val();
   if (regEx.test(input)){
     chef.set("challenge-2-question-1","true");
+    return true;
   }
-  return regEx.test(input);
+  else{
+    return false;
+  }
 }
 
 //button calls for validation of question 1
 $("#q-1-button").on("click",function(){
-  var pop = $('#q-1-button').popover({content: "Try again!", });
-  var answer = $("#q-1-answer").val();
+  let pop = $('#q-1-button').popover({content: "Try again!", });
+  const answer = $("#q-1-answer").val();
   if (ValidateQuestionOne(answer)){
     pop.popover("dispose");
     pop.popover({content: "Awesome!",});
     pop.popover("show");
+    CheckAllQuestions();
   }
   else {
     pop.popover('show');
@@ -135,22 +159,27 @@ $("#q-1-button").on("click",function(){
 //validates question 2
 function ValidateQuestionTwo(input){
   const chef = B();
-  const regEx = new RegExp("(peek)","gi")
+  const regEx = new RegExp("(peek)","gi");
   //var answer = $("#q-2-answer").val();
   if (regEx.test(input)){
     chef.set("challenge-2-question-2","true");
+    return true;
   }
-  return regEx.test(input);
+  else{
+    return false;
+  }
 }
 
 //button calls for validaton of question 2
 $("#q-2-button").on("click",function(){
-  var pop = $('#q-2-button').popover({content: "Try again!",});
-  var answer = $("#q-1-answer").val();
-  if (ValidateQuestionTwo()){
+  let pop = $('#q-2-button').popover({content: "Try again!",});
+  const answer = $("#q-2-answer").val();
+  console.log(answer);
+  if (ValidateQuestionTwo(answer)){
     pop.popover("dispose");
     pop.popover({content: "You got it!",});
     pop.popover("show");
+    CheckAllQuestions();
   }
   else{
     pop.popover("show");
@@ -159,25 +188,29 @@ $("#q-2-button").on("click",function(){
 });
 
 //validates question 3
-function ValidateQuestionThree(){
+function ValidateQuestionThree(input){
   const chef = B();
-  const regEx = new RegExp("(?=.*single)(?=.*lane)(?=.*road)|(?=.*one)(?=.*way)(?=.*road)","gi")
-  const answer = $("#q-3-answer").val();
-  if (regEx.test(answer)){
+  const regEx = new RegExp("(?=.*single)(?=.*lane)(?=.*road)|(?=.*one)(?=.*way)(?=.*road)","gi");
+  //const answer = $("#q-3-answer").val();
+  if (regEx.test(input)){
     chef.set("challenge-2-question-3","true");
+    return true;
   }
-  return regEx.test(answer);
+  else{
+    return false;
+  }
 }
 
 //button calls for validaton of question 3
 $("#q-3-button").on("click",function(){
-  var c = C();
-  var pop = $('#q-3-button').popover({content: "Try again!",});
-  if (ValidateQuestionThree()){
+  let pop = $('#q-3-button').popover({content: "Try again!",});
+  const answer = $("#q-3-answer").val();
+  if (ValidateQuestionThree(answer)){
     pop.popover("dispose");
     //SuccessMessageCode(c.mID.c,2);
     pop.popover({content: "You got it!",});
     pop.popover("show");
+    CheckAllQuestions(); 
   }
   else{
     pop.popover("show");
